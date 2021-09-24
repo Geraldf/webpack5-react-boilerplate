6f026ed89cfb2f0700a525c04fbeb0b9f804fe2d
Gerald Fuchs <gerald@fuchsclan.de>
2021-09-24 09:17:13 +0200

    Initial Working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,70 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
+import { useSelector } from "react-redux";
+import { Provider } from "react-redux";
 
 import "./app.styles.scss";
+import Home from "./containers/Home/Home";
+import Setting from "./containers/Setting/Setting";
+import ErrorBoundary from "./components/ErrorBoundaries";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        Webpack 5 boilerplate for react using babel, sass, with a hot dev server
-        and an optimized production build.
-      </div>
-    );
-  }
+import MainLayout from "./layouts/MainLayout";
+import EmptyLayout from "./layouts/EmptyLayout";
+
+import { getTheme } from "./containers/Setting/settingsReducer";
+
+const NotFound = () => {
+  return <div>NotFound</div>;
+};
+
+const DashboardRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(matchProps) => (
+        <MainLayout>
+          <Component {...matchProps} />
+        </MainLayout>
+      )}
+    />
+  );
+};
+
+const EmptyRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(matchProps) => (
+        <EmptyLayout>
+          <Component {...matchProps} />
+        </EmptyLayout>
+      )}
+    />
+  );
+};
+function App() {
+  const theTheme = useSelector(getTheme);
+  return (
+    <ErrorBoundary>
+      <MuiThemeProvider theme={createTheme(theTheme)}>
+        <CssBaseline />
+
+        <div style={{ height: "100vh" }}>
+          <Router>
+            <Switch>
+              <DashboardRoute path="/dashboard" component={Home} />
+              <DashboardRoute path="/setting" component={Setting} />
+              <DashboardRoute exact path="/" component={Home} />
+              <EmptyRoute component={NotFound} />
+            </Switch>
+          </Router>
+        </div>
+      </MuiThemeProvider>
+    </ErrorBoundary>
+  );
 }
+
 export default App;
